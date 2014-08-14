7a4c1851f829a060d9efe5ebc7c4983fd0bbcf95
Deon Poncini <dex1337@gmail.com>
2014-08-13 23:59:44 -0700

    Add more reliable card parsing with html parsing

--- data/scrape_cards.py
+++ data/scrape_cards.py
@@ -1,37 +1,18 @@
 #!/usr/bin/python
 
-import logging
-import rdflib
+import requests
 import codecs
+import re
 
 from urllib2 import quote
-
-logging.basicConfig()
+from bs4 import BeautifulSoup
 
 INPUT_FILE = 'cardunique.txt'
 ERROR_FILE = 'carderrors.txt'
 OUTPUT_FILE = 'card.txt'
 
 # yugioh wiki paths
-URI_PATH = 'http://yugioh.wikia.com/wiki/Special:URIResolver/'
-PROPERTY_PATH = URI_PATH + 'Property-3A'
-LOAD_PATH = 'http://yugioh.wikia.com/wiki/Special:ExportRDF/'
-
-# attribute maps:
-CARD_NAME = PROPERTY_PATH + 'English_name'
-CARD_TYPE = PROPERTY_PATH + 'Card_category_Text'
-ATTRIBUTE = PROPERTY_PATH + 'Attribute_Text'
-MONSTER_TYPE = PROPERTY_PATH + 'Card_type_Text'
-TYPE = PROPERTY_PATH + 'Types'
-LEVEL = PROPERTY_PATH + 'Level'
-RANK = PROPERTY_PATH + 'Rank'
-ATK = PROPERTY_PATH + 'ATK'
-ATKSTR = PROPERTY_PATH + 'ATK_string'
-DEF = PROPERTY_PATH + 'DEF'
-DEFSTR = PROPERTY_PATH + 'DEF_string'
-PENDULUM = PROPERTY_PATH + 'Pendulum_Scale'
-IMAGE = PROPERTY_PATH + 'Card_Image'
-TEXT = PROPERTY_PATH + 'Lore'
+LOAD_PATH = 'http://yugioh.wikia.com/wiki/'
 
 cardin = codecs.open(INPUT_FILE,'r','utf-8')
 cardout = codecs.open(OUTPUT_FILE,'w','utf-8')
@@ -55,46 +36,13 @@ def print_output(data):
           data['spellType'] + '\t' + \
           data['trapType'] + '\n')
 
-def check_type(t, monsterType):
-    # we don't know what type is present
-    if t == monsterType:
-        return ('monsterType', monsterType)
-    elif t == 'Effect':
-        return ('monsterAbility', t)
-    elif t == 'Flip':
-        return ('monsterAbility', t)
-    elif t == 'Toon':
-        return ('monsterAbility', t)
-    elif t == 'Spirit':
-        return ('monsterAbility', t)
-    elif t == 'Union':
-        return ('monsterAbility', t)
-    elif t == 'Gemini':
-        return ('monsterAbility', t)
-    elif t == 'Tuner':
-        return ('monsterAbility', t)
-    else:
-        # its a type
-        return ('type', t)
-
 def parse_monster(data):
     output = {}
     output['name'] = data['name']
     output['cardType'] = 'Monster'
     output['attribute'] = data['attribute']
-
     output['monsterType'] = data['monsterType'].split()[0]
-
-    if 'type0' in data:
-        (index, value) = check_type(data['type0'],output['monsterType'])
-        output[index] = value
-    if 'type1' in data:
-        (index, value) = check_type(data['type1'],output['monsterType'])
-        output[index] = value
-    if 'type2' in data:
-        (index, value) = check_type(data['type2'],output['monsterType'])
-        output[index] = value
-
+    output['type'] = data['type']
     output['level'] = data['level']
     output['attack'] = data['attack']
     output['defense'] = data['defense']
@@ -111,6 +59,8 @@ def parse_monster(data):
 
     if not 'monsterAbility' in data:
         output['monsterAbility'] = 'None'
+    else:
+        output['monsterAbility'] = data['monsterAbility']
 
     return output
 
@@ -153,65 +103,87 @@ def parse_trap(data):
 def parse_card(cardname):
     origcardname = cardname;
     cardname = cardname.replace(' ','_').replace('#','').strip()
-    g = rdflib.Graph()
-    uri = LOAD_PATH + cardname
-    g.load(LOAD_PATH + quote(cardname.encode('utf-8')))
+    uri = LOAD_PATH + quote(cardname.encode('utf-8'))
+    print uri
 
-    # load up the data dictionary
+    r = requests.get(uri)
+    html = r.text
+    soup = BeautifulSoup(html)
+    contents = soup.find_all("table",{"class":"cardtable"})
+
     data = {}
-    type_count = 0
+    data['name'] = origcardname
 
-    for s,p,o in g:
-        try:
-            k = str(p)
-            if k == CARD_NAME:
-                data['name'] = o.encode('utf-8')
-            elif k == CARD_TYPE:
-                data['cardType'] = str(o)
-            elif k == ATTRIBUTE:
-                data['attribute'] = str(o)
-            elif k == MONSTER_TYPE:
-                data['monsterType'] = str(o)
-            elif k == TYPE:
-                data['type' + str(type_count)] = str(o)
-                type_count = type_count + 1
-            elif k == LEVEL:
-                data['level'] = int(float(o))
-            elif k == RANK:
-                data['level'] = int(float(o))
-            elif k == ATK:
-                data['attack'] = int(float(o))
-            elif k == ATKSTR:
-                if o.isnumeric():
-                    data['attack'] = int(float(o))
-                else:
-                    data['attack'] = str(o)
-            elif k == DEF:
-                data['defense'] = int(float(o))
-            elif k == DEFSTR:
-                if o.isnumeric():
-                    data['defense'] = int(float(o))
-                else:
-                    data['defense'] = str(o)
-            elif k == PENDULUM:
-                data['lpendulum'] = int(float(o))
-                data['rpendulum'] = int(float(o))
-        except ValueError:
-            continue
+    if len(contents) == 0:
+        # do a retry with _(card) disambiguation suffix
+        uri = uri + '_(card)'
+        r = requests.get(uri)
+        html = r.text
+        soup = BeautifulSoup(html)
+        contents = soup.find_all("table",{"class":"cardtable"})
+
+    if len(contents) == 0:
+        log_error(origcardname)
+        return # we have truly failed
+
+    for tag in contents:
+        trTags = tag.find_all("tr")
+        for trTag in trTags:
+            thTags = trTag.find_all("th")
+            tdTags = trTag.find_all("td")
+            headers = []
+            info = []
+            for thTag in thTags:
+                headers.append(thTag.text.strip())
+            for tdTag in tdTags:
+                info.append(tdTag.text.strip())
+            if len(headers) == 1 and len(info) == 1:
+                if headers[0].strip() == 'Attribute':
+                    data['cardType'] = 'Monster'
+                    data['attribute'] = info[0].strip()
+                elif headers[0].strip() == 'Type':
+                    if info[0].strip() == 'Spell Card':
+                        data['cardType'] = 'Spell'
+                    elif info[0].strip() == 'Trap Card':
+                        data['cardType'] = 'Trap'
+                    else:
+                        data['type'] = info[0].strip()
+                        data['monsterType'] = 'Normal'
+                elif headers[0].strip() == 'Types':
+                    types = re.split(r'/', info[0].strip())
+                    data['type'] = types[0]
+                    data['monsterType'] = types[1]
+                    if len(types) == 3:
+                        data['monsterAbility'] = types[2]
+                elif headers[0].strip() == 'Level':
+                    data['level'] = int(float(info[0]))
+                elif headers[0].strip() == 'Rank':
+                    data['level'] = int(float(info[0]))
+                elif headers[0].strip() == 'Property':
+                    data['type0'] = info[0].strip()
+                elif headers[0].strip() == 'ATK/DEF':
+                    points = re.split(r'/', info[0].strip())
+                    if points[0].isnumeric():
+                        data['attack'] = int(float(points[0]))
+                    else:
+                        data['attack'] = points[0]
+                    if points[1].isnumeric():
+                        data['defense'] = int(float(points[1]))
+                    else:
+                        data['defense'] = points[1]
+                elif headers[0].strip() == 'Pendulum Scale':
+                    data['lpendulum'] = info[0].strip()
+                    data['rpendulum'] = info[0].strip()
 
     # process the output
-    try:
-        print data
-        if "cardType" in data and "Monster" in data['cardType']:
-            print_output(parse_monster(data))
-        elif "attribute" in data and "Spell" in data['attribute']:
-            print_output(parse_spell(data))
-        elif "attribute" in data and "Trap" in data['attribute']:
-            print_output(parse_trap(data))
-        else:
-            log_error(origcardname)
-    except:
-            log_error(origcardname)
+    if data['cardType'] == 'Monster':
+        print_output(parse_monster(data))
+    elif data['cardType'] == 'Spell':
+        print_output(parse_spell(data))
+    elif data['cardType'] == 'Trap':
+        print_output(parse_trap(data))
+    else:
+        log_error(origcardname)
 
 
 def parse_cards():
