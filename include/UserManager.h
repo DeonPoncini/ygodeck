038a1091d478087b02238af868caa3a5abe5152a
Deon Poncini <dex1337@gmail.com>
2014-08-23 17:07:19 -0700

    Allow deck sets from users

--- include/UserManager.h
+++ include/UserManager.h
@@ -1,8 +1,8 @@
 #ifndef YGO_USERMANAGER_H
 #define YGO_USERMANAGER_H
 
-#include "DB.h"
 #include "DBUser.h"
+#include "DBDeckSet.h"
 
 namespace ygo
 {
@@ -14,9 +14,7 @@ public:
     UserManager();
 
     DBUser login(const std::string& name);
-
-private:
-    DB mDB;
+    std::vector<DBDeckSet> deckSets(const DBUser& user);
 };
 
 }
--- src/UserManager.cpp
+++ src/UserManager.cpp
@@ -1,11 +1,13 @@
 #include "UserManager.h"
+
+#include "DB.h"
 #include "DBCommon.h"
+#include "Serialize.h"
 
 namespace ygo
 {
 
-UserManager::UserManager() :
-    mDB(DBPATH)
+UserManager::UserManager()
 {
 }
 
@@ -14,7 +16,8 @@ DBUser UserManager::login(const std::string& name)
     // check if the user already exists
     auto exists = false;
     std::string userid;
-    mDB.select(DBKeyList({"name","user_id"}),"users",
+    DB db(DBPATH);
+    db.select(DBKeyList({"name","user_id"}),"users",
             DBPair("name",name),
             [&](DB::DataMap data)
             {
@@ -28,8 +31,39 @@ DBUser UserManager::login(const std::string& name)
     }
 
     // create a new user
-    userid = mDB.insert("users",DBList({name}));
+    userid = db.insert("users",DBList({name}));
     return {name,userid};
+}
+
+std::vector<DBDeckSet> UserManager::deckSets(const DBUser& user)
+{
+    // return all deck sets for a given user
+    std::vector<std::string> deckids;
+    DB db(DBPATH);
+    db.select("deck_set_id","user_to_decks",
+            DBPair("user_id",user.id()),
+            [&](DB::DataMap data)
+            {
+                deckids.push_back(data["deck_set_id"]);
+            });
+
+    // look up the deck set ids and convert them into objects
+    std::vector<DBDeckSet> ret;
+    for (auto&& i : deckids)
+    {
+        db.select(DBAll(),"deck_set",
+                DBPair("deck_set_id",i),
+                [&](DB::DataMap data)
+                {
+                    // extract the format
+                    DBFormat f(toFormat(data["format"]),
+                        data["format_date"]);
+
+                    // add a new DBDeckSet
+                    ret.emplace_back(DBDeckSet{data["name"],user,f});
+                });
+    }
+    return ret;
 }
 
 }
