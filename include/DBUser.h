5306e93c8f642e7cbeb779f55618abf9404d0ce6
Deon Poncini <dex1337@gmail.com>
2014-08-23 17:14:58 -0700

    UserManager not needed
    All operations can be carried out inside DBUser

--- include/DBUser.h
+++ include/DBUser.h
@@ -2,17 +2,22 @@
 #define YGO_DBUSER_H
 
 #include <string>
+#include <vector>
 
 namespace ygo
 {
 
+class DBDeckSet;
+
 class DBUser
 {
 public:
-    DBUser(std::string name, std::string id);
+    DBUser(std::string name, bool create = false);
 
     std::string name() const { return mName; }
     std::string id() const { return mID; }
+
+    std::vector<DBDeckSet> deckSets(const DBUser& user);
 
 private:
     std::string mName;
--- src/DBUser.cpp
+++ src/DBUser.cpp
@@ -1,12 +1,73 @@
 #include "DBUser.h"
 
+#include "DB.h"
+#include "DBCommon.h"
+#include "DBDeckSet.h"
+#include "Serialize.h"
+
+#include <stdexcept>
+
 namespace ygo
 {
 
-DBUser::DBUser(std::string name, std::string id) :
-    mName(std::move(name)),
-    mID(std::move(id))
+DBUser::DBUser(std::string name, bool create) :
+    mName(std::move(name))
 {
+    // check if the user already exists
+    auto exists = false;
+    std::string userid;
+    DB db(DBPATH);
+    db.select(DBKeyList({"name","user_id"}),"users",
+            DBPair("name",mName),
+            [&](DB::DataMap data)
+            {
+                exists = true;
+                userid = data["user_id"];
+            });
+
+    if (exists)
+    {
+        mID = userid;
+    }
+    else if (create)
+    {
+        mID = db.insert("users",DBList({mName}));
+    }
+    else
+    {
+        throw std::runtime_error("User " + mName + " does not exist");
+    }
+}
+
+std::vector<DBDeckSet> DBUser::deckSets(const DBUser& user)
+{
+    // return all deck sets for a given user
+    std::vector<std::string> deckids;
+    DB db(DBPATH);
+    db.select("deck_set_id","user_to_decks",
+            DBPair("user_id",user.id()),
+            [&](DB::DataMap data)
+            {
+                deckids.push_back(data["deck_set_id"]);
+            });
+
+    // look up the deck set ids and convert them into objects
+    std::vector<DBDeckSet> ret;
+    for (auto&& i : deckids)
+    {
+        db.select(DBAll(),"deck_set",
+                DBPair("deck_set_id",i),
+                [&](DB::DataMap data)
+                {
+                    // extract the format
+                    DBFormat f(toFormat(data["format"]),
+                        data["format_date"]);
+
+                    // add a new DBDeckSet
+                    ret.emplace_back(DBDeckSet{data["name"],user,f});
+                });
+    }
+    return ret;
 }
 
 }
