5eb77b0826baaf951d43d3fb6a3493fb28e24896
Deon Poncini <dex1337@gmail.com>
2015-01-25 11:14:48 -0800

    Move example and binding to separate repositories
    For scalability its best to have bindings and example applications in
    separate repositories
    
    This means people don't need all the dependencies to actually build the
    applications

--- binding/java/src/net/sectorsoftware/ygo/deck/DB.java
+++
@@ -1,8 +0,0 @@
-package net.sectorsoftware.ygo.deck;
-
-public class DB
-{
-    public static native void setPath(String path);
-
-    public static native String getPath();
-}
--- example/android/src/com/example/ygodeck/DeckContent.java
+++
@@ -1,9 +0,0 @@
-package com.example.ygodeck;
-
-import java.util.ArrayList;
-
-public interface DeckContent {
-    public void addCards(ArrayList<String> cards);
-    public void addCard(String card);
-    public void removeCard(String card);
-}
